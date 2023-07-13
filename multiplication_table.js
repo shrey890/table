822a3458c0b9af339de066cb57eb1d53383aeb80
SHREY PATEL <134781442+shrey890@users.noreply.github.com>
2023-07-13 13:19:51 +0530

    Add files via upload

---
+++ multiplication_table.js
@@ -0,0 +1,20 @@
+let number = parseInt(prompt('enter the number to get the multiplication table'))
+// console.log(number);
+
+if (!isNaN(number)) {
+    // for(let i=1; i<=10;i++){
+    //     let res=number + "*" + i + "=" + (number*i);
+    //     console.log(res);
+
+    // console.log('Multiplication table for ' + number + ':');
+    let ul = '<ul>'
+    for (let i = 1; i <= 10; i++) {
+        let res = number + ' * ' + i + ' = ' + (number * i);
+        ul+='<li>' + res + '</li>'
+    }
+    ul +='</ul>';
+    document.write(ul)
+} else {
+    alert('Please enter a number')
+
+}
